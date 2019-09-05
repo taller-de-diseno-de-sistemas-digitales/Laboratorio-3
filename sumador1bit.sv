45495e50b1a07929d4dfe86a57c1bdac1c3d6ef9
AllanCh <edochacon96@gmail.com>
2019-09-04 20:43:23 -0600

    sumador1bit

---
+++ sumador1bit.sv
@@ -0,0 +1,11 @@
+module sumador1bit (input logic a,b,cin
+							output logic s, cout);
+								
+logic s1,s2,s3;
+xor n1(s1,a,b);
+xor n2(s,s1,cin);
+and n3(s2,cin,s1);
+and n4(s3,a,b);
+or  n5(cout,cin,s2);
+
+endmodule 
