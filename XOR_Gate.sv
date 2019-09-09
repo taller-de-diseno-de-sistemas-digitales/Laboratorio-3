43226cce46b726721eb9e92a950252bc03360ec2
AllanCh <edochacon96@gmail.com>
2019-09-09 10:12:58 -0600

    Compuerta XOR
    Parametrizacion de la compuerta XOR

--- XOR_Gate.sv
+++ XOR_Gate.sv
@@ -1,5 +1,12 @@
-module XOR_Gate(input a,b,
-				output s);	
-xor n1(s,a,b);
+module XOR_Gate#(parameter N=1)
+(input [N-1:0] a,b,
+ output [N-1:0] s
+ );	
+genvar i;
+generate
+	for(i=0; i<N; i=i+1) begin: generate_block
+		xor n1(s[i],a[i],b[i]);
+	end
+endgenerate	
 endmodule
 
